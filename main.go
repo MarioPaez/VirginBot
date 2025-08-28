e30c2ddb4650d9a5c5c10faae2b44f2df6316671
SF53151 <mario.paez@external.stellantis.com>
2025-08-28 18:31:12 +0200

    added chromedp library to sign in

--- main.go
+++ main.go
@@ -1,75 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"net/http/cookiejar"
-	"io"
-	"net/url"
-	"regexp"
-	"os"
-	"strings"
-)
+import "github.com/MarioPaez/VirginBot/session"
 
 func main() {
-	// Crear un cliente HTTP con almacenamiento de cookies
-	jar, _ := cookiejar.New(nil)
-	client := &http.Client{Jar: jar}
-
-	// URL de la página de login
-	loginPageURL := "https://shop.virginactive.it/account/login"
-	fmt.Println("URL de la página de login:", loginPageURL)
-	// 1️⃣ Obtener el _csrf_token desde la página de login
-	resp, err := client.Get(loginPageURL)
-	if err != nil {
-		fmt.Println("Error al obtener la página de login:", err)
-		return
-	}
-	defer resp.Body.Close()
-	
-	// Leer el contenido de la respuesta
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error al leer la respuesta:", err)
-		return
-	}
-	// Buscar el token CSRF en el HTML usando una expresión regular
-	csrfRegex := regexp.MustCompile(`<input type="hidden" name="_csrf_token" value="(.*?)">`)
-	matches := csrfRegex.FindStringSubmatch(string(body))
-	if len(matches) < 2 {
-		fmt.Println("No se encontró el token CSRF")
-		return
-	}
-	csrfToken := matches[1]
-	fmt.Println("CSRF Token encontrado:", csrfToken)
-
-	// 2️⃣ Enviar la petición POST con el login
-	loginURL := "https://shop.virginactive.it/account/login"
-	// Acceder a las variables
-	email := os.Getenv("VA_EMAIL")
-	password := os.Getenv("VA_PASS")
-
-	data := url.Values{
-		"_csrf_token": {csrfToken},
-		"username":    {email},
-		"password":    {password},
-		"login":       {"Accedo"}, // Esto puede ser opcional, pero algunas webs lo requieren
-	}
-
-	req, _ := http.NewRequest("POST", loginURL, strings.NewReader(data.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Referer", loginPageURL) // Algunas webs requieren este header
-
-	resp, err = client.Do(req)
-	if err != nil {
-		fmt.Println("Error al enviar login:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	// 3️⃣ Verificar si se guardaron cookies
-	fmt.Println("Cookies guardadas tras el login:")
-	for _, cookie := range jar.Cookies(req.URL) {
-		fmt.Println(cookie.Name, ":", cookie.Value)
-	}
+	session.DoLogin()
 }
---
+++ session/session.go
@@ -0,0 +1,56 @@
+package session
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/chromedp/chromedp"
+)
+
+const (
+	URL_LOGIN   = "https://shop.virginactive.it/account/login"
+	USER        = "VA_EMAIL"
+	PASS        = "VA_PASS"
+	CHROME_PATH = "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"
+)
+
+func DoLogin() {
+	user := os.Getenv(USER)
+	pass := os.Getenv(PASS)
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.ExecPath(CHROME_PATH),
+		chromedp.Headless,
+	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	ctx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate(URL_LOGIN),
+		chromedp.WaitVisible(`input[name="username"]`),
+		chromedp.SendKeys(`input[name="username"]`, user),
+		chromedp.SendKeys(`input[name="password"]`, pass),
+		chromedp.Click(`button[type="submit"]`),
+		chromedp.WaitVisible(`div.card-title`),
+	); err != nil {
+		log.Fatal("error trying during the sign in\n", err)
+	}
+	fmt.Println("signed in")
+}
+
+// Email
+// <input type="text" name="username" ng-required="true" ng-model="email" pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" value="">
+// pass
+// <input type="password" name="password" ng:model="password" required="required">
+//"C:\Program Files\Google\Chrome\Application\chrome.exe"
+//Espero este div al iniciar sesión:
+/*
+<div class="card-title">
+                        Gestione di abbonamenti
+                    </div>
+*/
